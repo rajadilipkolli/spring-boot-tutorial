9dec3ccdb505dae5af1010667278410dc267f906
Raja Dilip Kolli <rajadileepkolli@users.noreply.github.com>
2022-04-23 05:22:10 +0530

    attempt to upgrade

--- springboot2-xml-config/src/test/java/net/guides/springboot2/springboot2xmlconfig/Springboot2XmlConfigApplicationTests.java
+++ springboot2-xml-config/src/test/java/net/guides/springboot2/springboot2xmlconfig/Springboot2XmlConfigApplicationTests.java
@@ -1,16 +1,14 @@
 package net.guides.springboot2.springboot2xmlconfig;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+
 @SpringBootTest
-public class Springboot2XmlConfigApplicationTests {
+class Springboot2XmlConfigApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	void contextLoads() {
 	}
 
 }
