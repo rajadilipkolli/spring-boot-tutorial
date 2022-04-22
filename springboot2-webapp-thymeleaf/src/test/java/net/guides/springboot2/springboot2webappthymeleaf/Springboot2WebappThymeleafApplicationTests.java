87281d3a23d654e8d8782bc8ba88555bf4e4b39a
Raja Dilip Kolli <rajadileepkolli@users.noreply.github.com>
2022-04-23 04:11:20 +0530

    fixes issue with thymelead

--- springboot2-webapp-thymeleaf/src/test/java/net/guides/springboot2/springboot2webappthymeleaf/Springboot2WebappThymeleafApplicationTests.java
+++ springboot2-webapp-thymeleaf/src/test/java/net/guides/springboot2/springboot2webappthymeleaf/Springboot2WebappThymeleafApplicationTests.java
@@ -1,16 +1,13 @@
 package net.guides.springboot2.springboot2webappthymeleaf;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
-public class Springboot2WebappThymeleafApplicationTests {
+class Springboot2WebappThymeleafApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	void contextLoads() {
 	}
 
 }
