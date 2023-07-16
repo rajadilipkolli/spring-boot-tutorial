7e628ed4cc72450ca448a5b713d9efb60143f448
Raja Dilip Kolli <rajadileepkolli@gmail.com>
2023-07-16 19:45:25 +0000

    fixes issue with spring jpa auditing

--- springboot2-jpa-auditing/src/test/java/net/guides/springboot/springboot2jpaauditing/Springboot2JpaAuditingApplicationTests.java
+++ springboot2-jpa-auditing/src/test/java/net/guides/springboot/springboot2jpaauditing/Springboot2JpaAuditingApplicationTests.java
@@ -3,7 +3,7 @@ package net.guides.springboot.springboot2jpaauditing;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = TestSpringboot2JpaAuditingApplication.class)
 public class Springboot2JpaAuditingApplicationTests {
 
 	@Test
